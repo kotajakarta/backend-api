0ef645aae0469661c802b3b37afc242317d382c4
aithendi <aithendi@example.com>
2026-07-03 10:24:18 +0700

    Update backend-api code

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /data/podman-hosting/apps/backend-api
-git pull origin main
+git fetch origin main
+git reset --hard origin/main
+git clean -fd
 podman exec -it daimi-api npm install --production
 podman exec -it daimi-api npx prisma generate
 podman exec -it daimi-api npx prisma migrate deploy
