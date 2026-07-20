54088a4c95542c5882f80f463e79b4162ec7cb54
aithendi <aithendi@example.com>
2026-07-20 09:54:17 +0700

    Update backend-api code

--- dev.sh
+++ dev.sh
@@ -17,7 +17,7 @@ git push origin main
 
 
 # Script untuk menjalankan backend-api secara lokal untuk pengembangan
-echo "=== Menghentikan proses di port 8080 (jika ada) ==="
+
 fuser -k 8080/tcp 2>/dev/null || kill -9 $(lsof -t -i:8080) 2>/dev/null || true
 echo "=== Menjalankan Backend API Lokal ==="
 npm run dev
